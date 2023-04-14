7715c47fed797b5882acffb950c082763d010893
zamaphiri22@gmail.com <zamaphiri22@gmail.com>
2023-04-14 16:17:51 -0700

    Added Search Function

--- Book_Manag_Sys.py
+++ Book_Manag_Sys.py
@@ -1,1 +1,15 @@
-
+# Function to search for a book in the text file
+def search_book():
+    search_cat = input("search for book by category, author, genre or isbn: ")
+    
+    try:
+        with open(book_path, "r") as file:
+            found = False
+            for line in file:
+                if search_cat in line:
+                    found = True
+                    print(line.strip())
+            if not found:
+                print("Book not found in the library.")
+    except FileNotFoundError:
+        print("No books found in the library.")
